687ae1c16cd946ade785f9141d4dc7487b74dab6
Holger Brandl <brandl@extern.mpi-cbg.de>
2018-10-04 01:54:02 +0200

    cont demo script

--- src/main/kotlin/WildRideThroughNYC.kts
+++ src/main/kotlin/WildRideThroughNYC.kts
@@ -8,9 +8,11 @@
 import krangl.*
 import krangl.experimental.oneHot
 import kravis.*
+import kravis.OrderUtils.reorder
 import ml.dmlc.xgboost4j.java.DMatrix
 import ml.dmlc.xgboost4j.java.XGBoost
 import java.io.File
+import java.lang.Math.*
 import java.time.DayOfWeek
 import java.time.LocalDateTime
 import java.time.Month
@@ -92,10 +94,31 @@ fun prepareFeatures(trainData: DataFrame): DataFrame {
 //toto join with weather data
     // calculate the distance of the trip
 
-    trainData = trainData.addColumn(distance) { df ->
-        val longDist = df[pickup_longitude] - df[dropoff_longitude]
-        val latDist = df[pickup_latitude] - df[dropoff_latitude]
-        (longDist * longDist + latDist * latDist).asDoubles().map { Math.sqrt(it!!) }
+    //https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters
+    val coordDistance = fun(lon1: Double, lat1: Double, lon2: Double, lat2: Double): Double {
+        val R = 6378.137                                // radius of earth in Km
+        val dLat = (lat2 - lat1) * PI / 180
+        val dLon = (lon2 - lon1) * PI / 180
+        val a = pow(sin((dLat / 2)), 2.0) + cos(lat1 * PI / 180) * cos(lat2 * PI / 180) * pow(sin(dLon / 2), 2.0)
+        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        val d = R * c
+        return (d * 1000)                           // distance in meters
+    }
+
+//    trainData = trainData.addColumn(distance) { df ->
+//        val longDist = df[pickup_longitude] - df[dropoff_longitude]
+//        val latDist = df[pickup_latitude] - df[dropoff_latitude]
+//        (longDist * longDist + latDist * latDist).asDoubles().map { Math.sqrt(it!!) }
+//    }
+    trainData = trainData.addColumn(distance) {
+        df.rows.map { row ->
+            coordDistance(
+                row[pickup_longitude] as Double,
+                row[pickup_latitude] as Double,
+                row[dropoff_longitude] as Double,
+                row[dropoff_latitude] as Double
+            )
+        }
     }
 
     trainData = trainData.addColumns(
@@ -114,36 +137,95 @@ trainData = prepareFeatures(trainData)
 trainData.schema()
 
 
+//fun DataFrame.cleanup(): DataFrame = filter { it[trip_duration] lt 22 * 3600.0 }
+//        .filter { it[distance] gt 0.0 }
+//        .filter { it[trip_duration] gt 10.0 }
+//        .filter { it["speed"] lt 100.0 }
+
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Do some visual inspection
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-trainData.plot(x = passenger_count).geomBar()
-
-
+//' Analyze overal distribution of the trip duration
 //trainData.plot(trip_duration).geomHistogram()
 trainData.filter { it[trip_duration] lt 1000 }.plot(x = trip_duration).geomHistogram()
-
-trainData.count(passenger_count)
-    .plot(x = passenger_count.asDiscreteVariable, y = "n")
-    .geomBar(stat = Stat.identity)
+trainData.plot(x = passenger_count.asDiscreteVariable)
+    .geomBar()
     .xLabel("# Passengers")
 
 
+//' Is it really  NYC
+trainData.plot(x = pickup_longitude, y = pickup_latitude).geomPoint(alpha = .1)
+
 fun DataFrame.constrainCoord(): DataFrame = filter {
-    with(it[pickup_longitude]) { (this gt -74.1) AND (this lt -73.7) } AND
-        with(it[pickup_latitude]) { (this gt 40.55) AND (this lt 40.9) }
+    with(it[pickup_longitude]) { (this gt -74.05) AND (this lt -73.75) } AND
+        with(it[pickup_latitude]) { (this gt 40.6) AND (this lt 40.9) }
 }
 
-trainData.plot(x = pickup_longitude, y = pickup_latitude).geomPoint(alpha = .1)
+
 trainData
     .constrainCoord()
-    .plot(x = pickup_longitude, y = pickup_latitude).geomPoint(alpha = .1, size = .3)
+    .plot(x = pickup_longitude, y = pickup_latitude)
+    .geomPoint(alpha = .1, size = .3)
 
-trainData.addColumn("speed") { it[distance] / it[trip_duration] * 3.6 }
-//todo do heatmap
+//' LIVE@KC build a better version of the previous plot
+trainData
+    .constrainCoord()
+    .plot(x = pickup_longitude, y = pickup_latitude)
+    .geomBin2D(bins = 80)
 
+
+//' Correlate distance with trip duration (is it a promising predictor?)
+trainData
+    .sampleN(1e4.toInt())
+    .plot(distance, trip_duration)
+    .geomPoint()
+    .scaleYLog10().scaleXLog10()
+
+//trainData[distance].median()
+
+//' try again  but with binning
+trainData
+    .plot(distance, trip_duration)
+    .geomBin2D()
+    .scaleYLog10().scaleXLog10()
+
+
+//' Speed analysis
+trainData = trainData.addColumn("speed") { it[distance] / it[trip_duration] * 3.6 }
+
+trainData.plot(distance).geomHistogram()
+
+trainData.filter { (it["speed"] gt 2) AND (it["speed"] lt 1e2) }
+    .plot("speed")
+    .geomHistogram(fill = RColor.red, bins = 50)
+    .labs(x = "Average speed [km/h] (direct distance)")
+
+
+//' Speed analysis by day and hour
+trainData.addColumn("wday") { it["wday"].map<DayOfWeek> { it.value } }
+
+
+//Live@KC do heatmap
+
+trainData
+    .filter { it["speed"] lt 40.0 }
+    .plot("wday", "speed").geomBoxplot()
+
+trainData.filter { it["speed"] lt 40.0 }
+    .plot("hour".asDiscreteVariable, "speed").geomBoxplot()
+
+trainData
+//    .filter { (it["speed"] lt 40.0) AND (it[trip_duration] lt 3600) }
+//    .cleanup()
+    .groupBy("wday", "hour")
+    .summarize("median_speed" to { it["speed"].median() })
+    .addColumn("wday_order") { it["wday"].map<DayOfWeek> { it.value } }
+    .plot("hour", reorder("wday", "wday_order"), fill = "median_speed")
+    .geomTile()
+    .labs(x = "Hour of the day", y = "Day of the week")
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Build a predictive model with xgboost
@@ -178,7 +260,7 @@ var valMatDf = dataSplit.second
 //var trainMatDf =DataFrame.readCSV(File("trainMatDf.dump.txt"))
 
 fun DataFrame.selectPredictors(): DataFrame = select(
-    vendor_id, passenger_count, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, "dist", "month", "wday", "hour", "work"
+    passenger_count, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, distance, "month", "wday", "hour", "work"
 ).oneHot<Month>("month")
     .oneHot<DayOfWeek>("wday")
     .addColumn("work") { rows.map { if (it["work"] as Boolean) 1 else 0 } }
@@ -205,7 +287,6 @@ fun DataFrame.buildTrainMatrix(responseVariable: String = trip_duration): DMatri
 //trainMatDf.toFloatMatrix().reduce { left, right -> left + right }.size
 // Construct the training input for XGBoost
 val trainMat = trainMatDf.buildTrainMatrix()
-
 
 
 val params = hashMapOf<String, Any>().apply {
@@ -256,10 +337,11 @@ val testFeat = prepareFeatures(testData).selectPredictors().buildTestMatrix()
 
 var testPrediction = booster.predict(testFeat).map { it.first() }
 
-testData.schema()
 val kaggleSubmission = testData
     .addColumn("trip_duration") { testPrediction }
     .select("id", "trip_duration")
 
 kaggleSubmission.schema()
 kaggleSubmission.writeCSV(File("kotlin4kaggle.csv"))
+
+println("Finished first (out of N>>1) kaggle iteration using kotlin!")
